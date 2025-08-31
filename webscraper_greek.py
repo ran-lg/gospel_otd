dea0d8092e0041f2b6ff5aa8f5ee389defa8e441
rantien <tranthien.valery@gmail.com>
2025-08-31 17:33:28 +0200

    update webscraper_greek.py with timer

--- webscraper_greek.py
+++ webscraper_greek.py
@@ -2,20 +2,22 @@ import requests
 import re
 from os.path import isfile
 from os import listdir, mkdir
+from random import randint
+from time import sleep
 
 patterns = [r' <P><a name="...">..<\/A>&nbsp;', r' <P><a name="...">.<\/A>&nbsp;', r'<BR></P>', r'</P>']
 
 
-# get all text lines in a given page, e.g. 'https://sacred-texts.com/bib/vul/luk001.htm'
+# get all text lines in a given page, e.g. 'https://sacred-texts.com/bib/gnt/luk001.htm'
 
 def get_lines(url):
     r = requests.get(url)
     if r.status_code == '404':
         return []
-    else:    
+    else:
         lines = r.text.replace('</H3>', '</H3>\n ').replace('\n </P>\n', '</P>\n').replace('[', '').replace(']', '').split('\n')
         lines = [line for line in lines if line.startswith(' <P><a name=')]
-        
+
         new_lines = []
         for line in lines:
             for pattern in patterns:
@@ -35,12 +37,12 @@ def write_to_file(filename, new_lines, chapter):
             lines = f.readlines()
     else:
         lines = []
-        
+
     lines += [f'{chapter}|{new_lines.index(line) + 1}|{line}\n' for line in new_lines]
-    
+
     with open(filename, 'w', encoding = 'utf-8') as f:
         f.writelines(lines)
-    
+
 
 if __name__ == '__main__':
     urls = {"luc" : 'https://sacred-texts.com/bib/gnt/luk0',
@@ -58,11 +60,13 @@ if __name__ == '__main__':
             i_str = f'0{str(i)}'
         else:
             i_str = str(i)
-    
+
         file_created = False
         while get_lines(urls[evangelist] + f'{i_str}.htm') != []:
-            lines = get_lines(urls[evangelist] + f'{i_str}.htm')
-            
+            url = urls[evangelist] + f'{i_str}.htm'
+            print(url)
+            lines = get_lines(url)
+            sleep(randint(2, 10))
             write_to_file(filename, lines, i)
             i += 1
             if i < 10:
