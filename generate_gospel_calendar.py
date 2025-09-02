9d0cc745d39e0aec4136d912fc2648eebdfb4d6b
waleriant <tranthien.valery@gmail.com>
2025-09-02 15:00:00 +0200

    generate_gospel_calendar: alpha function added

--- generate_gospel_calendar.py
+++ generate_gospel_calendar.py
@@ -1,9 +1,15 @@
 from extract_gospel import get_gospel_otd
 from generate_urls import generate_urls_delta
+from sys import argv
 
 
 if __name__ == '__main__':
-    dict_list = generate_urls_delta(delta = 50)
+    try:
+        delta = int(argv[1])
+    except:
+        delta = 5
+
+    dict_list = generate_urls_delta(delta)
     for my_dict in dict_list:
         my_dict['gospel_otd'] = get_gospel_otd(my_dict['url'])
 
