883cd5d0210ea6159b72b3b34a32f40637897dd6
waleriant <tranthien.valery@gmail.com>
2025-09-02 15:01:25 +0200

    generate_gospel_calendar: alpha function added

--- generate_urls.py
+++ generate_urls.py
@@ -26,6 +26,23 @@ def generate_urls_delta(delta = 10):
     
     return days
 
+# if alpha = 2 and today's date is 25/08/2025,
+#
+#   generate [{'date': 2025-08-25, 'url': 'https://www.aelf.org/2025-08-25/romain/messe'}, 
+#             {'date': 2025-08-26, 'url': 'https://www.aelf.org/2025-08-26/romain/messe'}, 
+#             {'date': 2025-08-27, 'url': 'https://www.aelf.org/2025-08-27/romain/messe'}] 
+
+def generate_urls_alpha(alpha = 10):
+    today = datetime.datetime.now()
+    days = [{'date': today, 'url': 'https://www.aelf.org/'}]
+    
+    for i in range(1, alpha + 1):
+        day_x = today + i * datetime.timedelta(days = 1)
+        url_x = 'https://www.aelf.org/' + day_x.strftime('%Y-%m-%d') + '/romain/messe'
+        days.append({'date': day_x, 'url': url_x})
+    
+    return days
+
 
 if __name__ == '__main__':
     try:
