4a0b8461b50f84caac5d2c9fef74b23cb3786ff3
waleriant <tranthien.valery@gmail.com>
2025-09-02 19:42:12 +0200

    merged generation files

--- generate_gospel_calendar.py
+++ generate_gospel_calendar.py
@@ -1,6 +1,79 @@
-from extract_gospel import get_gospel_otd
-from generate_urls import generate_urls_delta
 from sys import argv
+import re
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+
+# if delta = 2 and today's date is 25/08/2025,
+#
+#   generate [{'date': 2025-08-23, 'url': 'https://www.aelf.org/2025-08-23/romain/messe'}, 
+#             {'date': 2025-08-24, 'url': 'https://www.aelf.org/2025-08-24/romain/messe'}, 
+#             {'date': 2025-08-25, 'url': 'https://www.aelf.org/2025-08-25/romain/messe'}, 
+#             {'date': 2025-08-26, 'url': 'https://www.aelf.org/2025-08-26/romain/messe'}, 
+#             {'date': 2025-08-27, 'url': 'https://www.aelf.org/2025-08-27/romain/messe'}] 
+
+def generate_urls_delta(delta = 10):
+    today = datetime.datetime.now()
+    days = [{'date': today, 'url': 'https://www.aelf.org/'}]
+    
+    for i in range(1, delta + 1):
+        day_x = today + i * datetime.timedelta(days = 1)
+        day_y = today - i * datetime.timedelta(days = 1)
+        
+        url_x = 'https://www.aelf.org/' + day_x.strftime('%Y-%m-%d') + '/romain/messe'
+        url_y = 'https://www.aelf.org/' + day_y.strftime('%Y-%m-%d') + '/romain/messe'
+        
+        days.append({'date': day_x, 'url': url_x})
+        days.append({'date': day_y, 'url': url_y})
+    
+    return days
+
+# if alpha = 2 and today's date is 25/08/2025,
+#
+#   generate [{'date': 2025-08-25, 'url': 'https://www.aelf.org/2025-08-25/romain/messe'}, 
+#             {'date': 2025-08-26, 'url': 'https://www.aelf.org/2025-08-26/romain/messe'}, 
+#             {'date': 2025-08-27, 'url': 'https://www.aelf.org/2025-08-27/romain/messe'}] 
+
+def generate_urls_alpha(alpha = 10):
+    today = datetime.datetime.now()
+    days = [{'date': today, 'url': 'https://www.aelf.org/'}]
+    
+    for i in range(1, alpha + 1):
+        day_x = today + i * datetime.timedelta(days = 1)
+        url_x = 'https://www.aelf.org/' + day_x.strftime('%Y-%m-%d') + '/romain/messe'
+        days.append({'date': day_x, 'url': url_x})
+    
+    return days
+
+# extracts the gospel for a given day url, e.g. 'Mt 23, 13-22'
+
+def get_gospel_otd(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    for li in soup.find_all('li'):
+        if '>Évangile<' in str(li):
+
+            regex1 = r'ref">(.*)<'
+            result = re.search(regex1, str(li))
+            my_str = result.group(1)
+            my_str = my_str.replace(u'\xa0', ' ')
+            my_str = my_str.replace('...', ' ')
+            my_str = my_str.replace('–', '-')
+            my_str = my_str.replace(', ', ',')
+           
+            my_str = my_str.replace(' - ', '-')
+            
+            while '  ' in my_str:
+                my_str = my_str.replace('  ', ' ')
+
+            regex2 = r' \(.*| \[.*'
+            for occurrence in re.findall(regex2, my_str):
+                my_str = my_str.replace(occurrence, '')
+
+            return my_str
+    return ''
 
 
 if __name__ == '__main__':
